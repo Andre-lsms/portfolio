0a7a725dd3a2a4ceb60045c14a3346db54f7ac26
André Santos <andreluismagalhaes14@gmail.com>
2025-07-09 10:18:02 -0300

    responsividade completa

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -6,41 +6,38 @@ import ae from "../../assets/icons/ae.png";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = () => {
+  const handleClick = () => {
     navigate("/projetos"); // rota para onde você quer ir
   };
   return (
     <div>
-      <div className="bg-secondary pt-10  ">
-        <div className="relative items-center flex flex-col mx-20">
-          {/* Container principal com largura responsiva */}
-          <div className="relative z-10 bg-offwhite w-[1000px] max-h-[800px] p-10 min-h-[500px] rounded-lg shadow-lg flex flex-col items-center justify-center mx-20 ">
-            {/* Texto com largura responsiva e padding lateral */}
-            <div className="  px-5 items-center flex flex-col">
-              <h1 className="text-8xl  text-primary mb-10 font-titulo font-bold">
-                Olá
-              </h1>
-              <p className="text-primary  text-xl font-sans max-w-[800px] text-center">
-                É um prazer apresentar meu portfólio. Reuni aqui trabalhos que
-                traduzem minha visão de design: simples, eficaz e centrado em
-                pessoas. Navegue com calma e me chame se quiser conversar sobre
-                um futuro projeto.
-              </p>
-            </div>
+      <div className="relative bg-secondary pt-10 h-[calc(100vh-7rem)] overflow-hidden  ">
+        <div className=" absolute z-10 top-[100%] bg-primary h-20 w-full -mt-20"></div>
+
+        <div className="absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 max-w-[80%] max-h-[90%] h-[90%] rounded-2xl bg-white shadow-2xl p-8 flex flex-col items-center justify-center">
+          {/* Texto com largura responsiva e padding lateral */}
+          <div className="  px-5 items-center flex flex-col">
+            <h1 className="text-5xl  sm:text-6xl lg:text-8xl text-primary mb-10 font-titulo font-bold">
+              Olá
+            </h1>
+            <p className="text-primary font-sans text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl max-w-[90%] sm:max-w-[80%] lg:max-w-[70%] text-justify leading-relaxed">
+              É um prazer apresentar meu portfólio. Reuni aqui trabalhos que
+              traduzem minha visão de design: simples, eficaz e centrado em
+              pessoas. Navegue com calma e me chame se quiser conversar sobre um
+              futuro projeto.
+            </p>
           </div>
         </div>
-
-        <div className="bg-primary h-[150px] w-full mt-[-100px] relative z-0"></div>
       </div>
 
-      <div className="bg-offwhite h-full w-full border-t-4 border-offwhite ">
+      <div className="bg-offwhite h-[calc(100vh)] w-full border-t-4 border-offwhite ">
         <div className="p-10 flex flex-col items-start px-20">
-          <h2 className="text-primary text-8xl font-titulo font-bold pb-10 self-center">
+          <h2 className="text-primary text-5xl  sm:text-6xl lg:text-8xl font-titulo font-bold pb-10 self-center">
             Trajetória
           </h2>
-          <p className="text-2xl text-primary font-sans font-s text-justify ">
+          <p className="text-primary font-sans text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-justify leading-relaxed">
             Desde pequeno, sempre tive o impulso de transformar ideias em algo
             visual. Foi ao buscar formas de dar vida aos meus projetos pessoais
             que conheci as ferramentas de design e me encantei com as
@@ -53,28 +50,44 @@ function Home() {
             comunicar com clareza e propósito, sempre apoiado pelas ferramentas
             que utilizo para dar vida aos meus projetos:
           </p>
-          <div className=" grid grid-cols-3 justify-items-center w-full pt-10">
-            <div className=" my-10 items-center flex flex-col">
-              <img src={ps} alt="Adobe Photoshop" className="w-28 " />
-              <span className="text-primary font-sans font-bold pt-3 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-8 justify-items-center w-full pt-10">
+            <div className="items-center flex flex-col">
+              <img
+                src={ps}
+                alt="Adobe Photoshop"
+                className="w-20 sm:w-24 md:w-28"
+              />
+              <span className="text-primary font-sans font-bold pt-3 text-center">
                 Adobe Photoshop
               </span>
             </div>
-            <div className=" my-10 items-center flex flex-col">
-              <img src={ai} alt="Adobe Illustrator" className="w-28 " />
-              <span className="text-primary font-sans font-bold pt-3 ">
+            <div className="items-center flex flex-col">
+              <img
+                src={ai}
+                alt="Adobe Illustrator"
+                className="w-20 sm:w-24 md:w-28"
+              />
+              <span className="text-primary font-sans font-bold pt-3 text-center">
                 Adobe Illustrator
               </span>
             </div>
-            <div className=" my-10 items-center flex flex-col">
-              <img src={pr} alt="Adobe Premiere" className="w-28 " />
-              <span className="text-primary font-sans font-bold pt-3 ">
+            <div className="items-center flex flex-col">
+              <img
+                src={pr}
+                alt="Adobe Premiere"
+                className="w-20 sm:w-24 md:w-28"
+              />
+              <span className="text-primary font-sans font-bold pt-3 text-center">
                 Adobe Premiere
               </span>
             </div>
-            <div className=" my-10 items-center flex flex-col">
-              <img src={ae} alt="Adobe After Effects" className="w-28 " />
-              <span className="text-primary font-sans font-bold pt-3 ">
+            <div className="items-center flex flex-col">
+              <img
+                src={ae}
+                alt="Adobe After Effects"
+                className="w-20 sm:w-24 md:w-28"
+              />
+              <span className="text-primary font-sans font-bold pt-3 text-center">
                 Adobe After Effects
               </span>
             </div>
@@ -82,12 +95,15 @@ function Home() {
         </div>
       </div>
       <div className="flex flex-col items-center bg-secondary">
-        <h1 className="text-primary font-titulo font-bold text-8xl py-10">
+        <h1 className="text-primary font-titulo font-bold text-5xl  sm:text-6xl lg:text-8xl  py-10">
           Alguns Posts
         </h1>
         <div className="flex flex-col">
           <Carrosel></Carrosel>
-          <button className="shadow shadow-primary self-center h-14 mb-5 text-primary " onClick={handleClick}>
+          <button
+            className="shadow shadow-primary self-center h-14 mb-5 text-primary "
+            onClick={handleClick}
+          >
             ver mais
           </button>
         </div>
