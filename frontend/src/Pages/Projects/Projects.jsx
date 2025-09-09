53e8248558c16a3a0dee43942e2b8bd363e68df4
André Santos <andreluismagalhaes14@gmail.com>
2025-09-09 18:04:38 -0300

    adicionando projetos
    removendo midias locais e instalando o supabase
    
    iniciando a conexão com o banco de dados

--- frontend/src/Pages/Projects/Projects.jsx
+++ frontend/src/Pages/Projects/Projects.jsx
@@ -1,36 +1,48 @@
-import { Link } from 'react-router-dom';
-import projectsData from '../../data/projects.json';
-
+import { Link } from "react-router-dom";
+import { useProjects } from "../../Hooks/useProjects"; // Verifique o caminho do seu hook
 
 function Projects() {
+  const { projects, loading, error } = useProjects();
+
+  if (loading)
+    return <div className="text-center py-40">Carregando projetos...</div>;
+  if (error)
+    return (
+      <div className="text-center py-40 text-red-500">
+        Erro ao carregar os projetos: {error}
+      </div>
+    );
+
   return (
-    <div className=" min-h-[calc(100vh-7rem)] py-16 px-4 sm:px-8">
+    <div className="bg-offwhite min-h-[calc(100vh-7rem)] py-16 px-4 sm:px-8">
       <div className="max-w-7xl mx-auto">
         <div className="bg-white rounded-2xl w-full p-6 sm:p-10 text-center shadow-lg">
           <h1 className="font-titulo font-bold text-3xl sm:text-4xl md:text-5xl text-primary mb-4">
             Meus Projetos
           </h1>
           <p className="text-primary max-w-3xl mx-auto font-sans text-lg leading-relaxed">
-            Cada projeto é uma oportunidade de explorar soluções que combinam funcionalidade e estética. Aqui compartilho trabalhos que representam minha trajetória e processo criativo.
+            Cada projeto é uma oportunidade de explorar soluções que combinam
+            funcionalidade e estética. Aqui compartilho trabalhos que
+            representam minha trajetória e processo criativo.
           </p>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-16">
-          {projectsData.map((projeto) => {
-            const primeiraImagemPath = projeto.entregas[0];
-            const imageUrl = `${primeiraImagemPath}`;
+          {projects.map((project) => {
             return (
-              <Link to={`/projetos/${projeto.slug}`} key={projeto.id}>
-                <div className="group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300">
+              <Link to={`/projetos/${project.slug}`} key={project.id}>
+                <div className="group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 h-full flex flex-col">
                   <div className="w-full h-80 overflow-hidden">
                     <img
-                      src={imageUrl}
-                      alt={`Capa do projeto ${projeto.titulo}`}
+                      src={project.thumb_url}
+                      alt={`Capa do projeto ${project.title}`}
                       className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
                     />
                   </div>
                   <div className="p-4 bg-white">
-                    <h3 className="font-titulo font-bold text-xl text-primary">{projeto.titulo}</h3>
+                    <h3 className="font-titulo font-bold text-xl text-primary">
+                      {project.title}
+                    </h3>
                   </div>
                 </div>
               </Link>
